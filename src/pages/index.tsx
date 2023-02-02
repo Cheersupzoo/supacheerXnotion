a9e4cb3cb89abb5b161bd609bdecd5d6d52fad03
cheer <cheersupzoo@gmail.com>
2023-02-03 01:52:03 +0700

    feat: use static build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home({ data }: { data: any }) {
   return <NotionRenderer recordMap={data} fullPage={true} darkMode={false} />;
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const notion = new NotionAPI();
   const recordMap = await notion.getPage(
     "Suppachai-a801d85fcc9e4c76bd7a4c60ad234952"
