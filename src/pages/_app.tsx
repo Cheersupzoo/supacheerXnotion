c80a3709dcfa5a26c516b49c7deac364d1917623
cheer <cheersupzoo@gmail.com>
2023-02-22 20:59:03 +0700

    feat: add posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,27 @@ import "styles/global.css";
 import "styles/notion.css";
 // global style overrides for prism theme (optional)
 import "styles/prism-theme.css";
+import posthog from "posthog-js";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    function onRouteChangeComplete() {
+      posthog.capture("$pageview");
+    }
+
+    posthog.init("phc_z2lv4tGxD4PeSIzFHY24YrJvHgzLz9hdm3vKffbi5Hh", {
+      api_host: "https://app.posthog.com",
+    });
+
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
