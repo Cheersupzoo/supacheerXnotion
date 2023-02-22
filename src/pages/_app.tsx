641f3a027837b2390d573ba253fab1a1ce731159
cheer <cheersupzoo@gmail.com>
2023-02-22 20:50:45 +0700

    feat: add basic css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,13 @@ import "prismjs/themes/prism-tomorrow.css";
 // used for rendering equations (optional)
 import "katex/dist/katex.min.css";
 import { AppProps } from "next/app";
+import "styles/global.css";
+// this might be better for dark mode
+// import 'prismjs/themes/prism-okaidia.css'
+// global style overrides for notion
+import "styles/notion.css";
+// global style overrides for prism theme (optional)
+import "styles/prism-theme.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
