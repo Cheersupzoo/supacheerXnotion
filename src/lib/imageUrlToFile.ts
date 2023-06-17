542f1603af8dd35aefc4970a0e237d94c2cf0097
cheer <cheersupzoo@gmail.com>
2023-06-17 22:04:47 +0700

    feat: compress image with webp

--- src/lib/imageUrlToFile.ts
+++ src/lib/imageUrlToFile.ts
@@ -5,13 +5,14 @@ import { extractKeyFromUrl } from './extractKeyFromUrl'
 
 export async function imageUrlToFile(url: string) {
   const [key, contentType] = extractKeyFromUrl(decodeURIComponent(url))
-  const fileName = `/picture_cache/${key}.${contentType}`
+  const overrideContentType = 'webp'
+  const fileName = `/picture_cache/${key}.${overrideContentType}`
   if (!existsSync(`./public${fileName}`)) {
     const response = await fetch(url)
     const arrayBuffer = await response.arrayBuffer()
     let buffer = Buffer.from(arrayBuffer)
 
-    if (['jpeg', 'jpg', 'png','webp'].includes(contentType ?? '')) {
+    if (['jpeg', 'jpg', 'png', 'webp'].includes(contentType ?? '')) {
       const Sharp = (await import('sharp')).default
 
       let sharp = await Sharp(buffer)
@@ -19,16 +20,12 @@ export async function imageUrlToFile(url: string) {
       const { width, height } = await sharp.metadata()
       if ((width ?? 0) > 2500 || (height ?? 0) > 1800) {
         sharp = sharp.resize({
-          width: 2500,
-          height: 1800,
+          width: 2800,
+          height: 2000,
           fit: 'inside'
         })
       }
-
-      if (['jpeg', 'jpg'].includes(contentType ?? '')) {
-        sharp = sharp.jpeg({ mozjpeg: true, quality: 75 })
-      }
-
+      sharp = sharp.webp({ quality: 60, alphaQuality: 80 })
       buffer = await sharp.withMetadata().toBuffer()
     }
     await fs.writeFile(`./public${fileName}`, buffer)
