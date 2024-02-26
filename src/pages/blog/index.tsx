289d919c1b3977831a2ed05e104a486ccd259874
cheer <cheersupzoo@gmail.com>
2024-02-26 11:16:50 +0700

    feat: add blog header

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,68 +1,75 @@
-import { Layout } from "@/components/Layout";
-import PageSelector from "@/components/Mdx/pageSelector";
-import PostCard from "@/components/Mdx/postCard";
-import { getAllPosts } from "@/lib/Mdx/api";
-import { GetStaticProps } from "next";
-import config from "../../../blog.config";
+import { GetStaticProps } from 'next'
+
+import { Layout } from '@/components/Layout'
+import PageSelector from '@/components/Mdx/pageSelector'
+import PostCard from '@/components/Mdx/postCard'
+import { getAllPosts } from '@/lib/Mdx/api'
+
+import config from '../../../blog.config'
 
 interface MyProp {
   posts: {
-    [key: string]: any;
-  };
-  prevPosts: any;
-  nextPosts: any;
-  numOfPage: number;
+    [key: string]: any
+  }
+  prevPosts: any
+  nextPosts: any
+  numOfPage: number
 }
-export default function Home({ posts, prevPosts, nextPosts, numOfPage }: MyProp) {
-  const isLocal = process.env.NODE_ENV === "development";
+export default function Home({
+  posts,
+  prevPosts,
+  nextPosts,
+  numOfPage
+}: MyProp) {
+  const isLocal = process.env.NODE_ENV === 'development'
 
   return (
+    <Layout>
+      <div className='h-8' />
+      <div className='mt-16 text-center text-3xl font-semibold text-[var(--fg-color)]'>Blog</div>
+      <div className='mx-auto mb-4 mt-12 w-5/6  sm:w-2/3 sm:px-4 md:max-w-xl'>
+        {posts
+          .filter((post: any) => {
+            return isLocal || !post.draft
+          })
+          .map((post: any) => (
+            <PostCard key={post.slug} post={post} />
+          ))}
+      </div>
 
-      <Layout >
-        <div className="mx-auto w-5/6 sm:w-2/3 md:max-w-xl  sm:px-4 mt-12 mb-4">
-          {posts
-            .filter((post: any) => {
-              return isLocal || !post.draft;
-            })
-            .map((post: any) => (
-              <PostCard key={post.slug} post={post} />
-            ))}
-        </div>
-
-        <PageSelector numOfPage={numOfPage} curPage={1}/>
-      </Layout>
-
-  );
+      <PageSelector numOfPage={numOfPage} curPage={1} />
+    </Layout>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const posts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "description",
-    "author",
-    "coverImage",
-    "coverImageAlt",
-    "coverImageHeight",
-    "coverImageWidth",
-    "excerpt",
-    "type",
-    "draft",
-  ]);
+    'title',
+    'date',
+    'slug',
+    'description',
+    'author',
+    'coverImage',
+    'coverImageAlt',
+    'coverImageHeight',
+    'coverImageWidth',
+    'excerpt',
+    'type',
+    'draft'
+  ])
 
-  const startIndex = 0;
-  const endIndex = config.postsPerPage;
-  const prevPosts = null;
-  const nextPosts = endIndex >= posts.length ? null : 2;
-  const numOfPage = Math.ceil(posts.length / config.postsPerPage);
+  const startIndex = 0
+  const endIndex = config.postsPerPage
+  const prevPosts = null
+  const nextPosts = endIndex >= posts.length ? null : 2
+  const numOfPage = Math.ceil(posts.length / config.postsPerPage)
 
   return {
     props: {
       posts: posts.slice(startIndex, endIndex),
       prevPosts,
       nextPosts,
-      numOfPage,
-    },
-  };
-};
+      numOfPage
+    }
+  }
+}
--- src/pages/blog/page/[page].tsx
+++ src/pages/blog/page/[page].tsx
@@ -1,35 +1,40 @@
-import { GetStaticProps } from "next";
-import config from "../../../../blog.config";
-import { useRouter } from "next/router";
-import React from "react";
-import { Layout } from "@/components/Layout";
-import PostCard from "@/components/Mdx/postCard";
-import PageSelector from "@/components/Mdx/pageSelector";
-import { getAllPosts } from "@/lib/Mdx/api";
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+import { Layout } from '@/components/Layout'
+import PageSelector from '@/components/Mdx/pageSelector'
+import PostCard from '@/components/Mdx/postCard'
+import { getAllPosts } from '@/lib/Mdx/api'
+
+import config from '../../../../blog.config'
+
 interface MyProp {
   posts: {
-    [key: string]: any;
-  };
-  prevPosts: any;
-  nextPosts: any;
-  numOfPage: number;
+    [key: string]: any
+  }
+  prevPosts: any
+  nextPosts: any
+  numOfPage: number
 }
 export default function Home({
   posts,
   prevPosts,
   nextPosts,
-  numOfPage,
+  numOfPage
 }: MyProp) {
-  const isLocal = process.env.NODE_ENV === "development";
-  const router = useRouter();
-  const curPage = parseInt(router.query.page?.toString() ?? "1");
+  const isLocal = process.env.NODE_ENV === 'development'
+  const router = useRouter()
+  const curPage = parseInt(router.query.page?.toString() ?? '1')
 
   return (
     <Layout>
-      <div className="mx-auto w-5/6 sm:w-2/3 md:max-w-xl  sm:px-4 mt-12 mb-4">
+      <div className='h-8' />
+      <div className='mt-16 text-center text-3xl font-semibold text-[var(--fg-color)]'>Blog</div>
+      <div className='mx-auto mb-4 mt-12 w-5/6 sm:w-2/3 sm:px-4 md:max-w-xl'>
         {posts
           .filter((post: any) => {
-            return isLocal || !post.draft;
+            return isLocal || !post.draft
           })
           .map((post: any) => (
             <PostCard key={post.slug} post={post} />
@@ -38,60 +43,60 @@ export default function Home({
 
       <PageSelector numOfPage={numOfPage} curPage={curPage} />
     </Layout>
-  );
+  )
 }
 
 type Params = {
   params: {
-    page: number;
-  };
-};
+    page: number
+  }
+}
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const posts = getAllPosts([
-    "title",
-    "date",
-    "slug",
-    "description",
-    "author",
-    "coverImage",
-    "coverImageAlt",
-    "coverImageHeight",
-    "coverImageWidth",
-    "excerpt",
-    "type",
-    "draft",
-  ]);
+    'title',
+    'date',
+    'slug',
+    'description',
+    'author',
+    'coverImage',
+    'coverImageAlt',
+    'coverImageHeight',
+    'coverImageWidth',
+    'excerpt',
+    'type',
+    'draft'
+  ])
 
-  const curPage = parseInt(params?.page?.toString() ?? "1");
+  const curPage = parseInt(params?.page?.toString() ?? '1')
 
-  const startIndex = 0 + (curPage - 1) * config.postsPerPage;
-  const endIndex = config.postsPerPage + (curPage - 1) * config.postsPerPage;
-  const prevPosts = null;
-  const nextPosts = endIndex >= posts.length ? null : 2;
-  const numOfPage = Math.ceil(posts.length / config.postsPerPage);
+  const startIndex = 0 + (curPage - 1) * config.postsPerPage
+  const endIndex = config.postsPerPage + (curPage - 1) * config.postsPerPage
+  const prevPosts = null
+  const nextPosts = endIndex >= posts.length ? null : 2
+  const numOfPage = Math.ceil(posts.length / config.postsPerPage)
 
   return {
     props: {
       posts: posts.slice(startIndex, endIndex),
       prevPosts,
       nextPosts,
-      numOfPage,
-    },
-  };
-};
+      numOfPage
+    }
+  }
+}
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(["slug"]);
-  const numOfPage = Math.ceil(posts.length / config.postsPerPage);
+  const posts = getAllPosts(['slug'])
+  const numOfPage = Math.ceil(posts.length / config.postsPerPage)
   return {
     paths: Array.from({ length: numOfPage }, (_, i) => i + 1).map((num) => {
       return {
         params: {
-          page: `${num}`,
-        },
-      };
+          page: `${num}`
+        }
+      }
     }),
-    fallback: false,
-  };
+    fallback: false
+  }
 }
